ee955c55e62fc090dee09a1140a083a04217d90b
Tjudd04 <140094202+Tjudd04@users.noreply.github.com>
2025-07-21 17:13:19 -0400

    Update packet_generation.py

--- inc/packet_generation.py
+++ inc/packet_generation.py
@@ -1,157 +1,151 @@
 import random
-import subprocess
-import selenium
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 import requests
+from requests.exceptions import RequestException
 
-#-------------------------------------------------------------------------------------------------------------------
-# This Python script is designed to generate both doH and non-doH traffic. It employs Chromium, a headless
-# version of Google Chrome, along with the Selenium and Requests libraries to perform the lookups. The script
-# maintains a list of 50 different websites. For doH lookups, the script utilizes two servers: Google's DNS
-# server and Cloudflare's DNS server. The process begins by randomly selecting a website and then determining
-# if the lookup will be doH or non-doH through a random true or false decision. For non-doH lookups, the driver
-# simply performs a regular lookup of the website. In case of a doH lookup, the script selects one of the
-# two doH servers and employs the Requests library to perform the lookup through that server. This approach
-# facilitates the tagging of traffic as doH due to the ability to identify the server's IP address. The process
-# repeats 500 times or until sufficient packets are captured by the tcpdump.
-#-------------------------------------------------------------------------------------------------------------------
-# DoH servers
+# --- Configuration Constants ---
+# Use constants for easier modification and readability.
+NUM_REQUESTS = 500
 
-
-doh_servers = [
-    "https://dns.google/resolve",
-    "https://cloudflare-dns.com/dns-query"
-]
-# Websites
-websites = [
-    "www.google.com",
-    "www.youtube.com",
-    "www.facebook.com",
-    "www.amazon.com",
-    "www.twitter.com",
-    "www.instagram.com",
-    "www.linkedin.com",
-    "www.pinterest.com",
-    "www.reddit.com",
-    "www.wikipedia.org",
-    "www.netflix.com",
-    "www.microsoft.com",
-    "www.ebay.com",
-    "www.nike.com",
-    "www.apple.com",
-    "www.yahoo.com",
-    "www.twitch.tv",
-    "www.vimeo.com",
-    "www.dropbox.com",
-    "www.airbnb.com",
-    "www.spotify.com",
-    "www.snapchat.com",
-    "www.hulu.com",
-    "www.walmart.com",
-    "www.github.com",
-    "www.slack.com",
-    "www.medium.com",
-    "www.canva.com",
-    "www.shopify.com",
-    "www.zillow.com",
-    "www.fandom.com",
-    "www.paypal.com",
-    "www.cnn.com",
-    "www.bbc.com",
-    "www.espn.com",
-    "www.forbes.com",
-    "www.cnbc.com",
-    "www.nytimes.com",
-    "www.huffpost.com",
-    "www.buzzfeed.com",
-    "www.nationalgeographic.com",
-    "www.stackoverflow.com",
-    "www.theguardian.com",
-    "www.mashable.com",
-    "www.indeed.com",
-    "www.engadget.com",
-    "www.wired.com",
-    "www.cnet.com"
-    
-]
-
-# Configure Chrome options running in headless mode
-options = webdriver.ChromeOptions()
-options.add_argument('--no-sandbox')
-options.add_argument("--headless")
-options.add_argument('--disable-dev-shm-usage')
-
-# Configures service and sets up the driver
-service = Service()
-driver = webdriver.Chrome(service=service, options=options)
-
-# Loops though 1000 different times to generate pacets
-for i in range(1000):
-#------------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------------------------
 #                                     Post Lab Exercise
-
-# Replace the value witin the if satement below from 50 to the values indicated in your lab manual and
+#
+# Replace the value stored in Doh_Packet_Percentage below from 50 to the values indicated in your lab manual and
 # rerun the lab
-#------------------------------------------------------------------------------------------------------
-    # Picks a random website from the list
-    ran_website = random.choice(websites)
+# --------------------------------------------------------------------------------------------------------------------
+# Set the probability of a request being DoH (e.g., 70 means 70%).
+DOH_PROBABILITY_PERCENT = 50
 
-    # Randomizes the choice between doH and non-doH lookups does 70% doH and 30% non-doH
-    doh = random.randint(1,100)
-    if doh <= 50 : # Change value from 50 to values indicated in lab manual
-        print("Performing DoH lookup:", ran_website)
-        # Chooses a random DoH server from the list
-        doh_serv = random.choice(doh_servers)
+# List of DoH servers, structured to handle different API requirements.
+DOH_SERVERS = [
+    {
+        "url": "https://dns.google/resolve",
+        "headers": None,
+        "params": lambda domain: {"name": domain, "type": "A"},
+    },
+    {
+        "url": "https://cloudflare-dns.com/dns-query",
+        "headers": {"accept": "application/dns-json"},
+        "params": lambda domain: {"name": domain, "type": "A"},
+    },
+]
 
-        # Checks if the server is google's, forms the url, and preforms the lookup
-        if doh_serv == "https://dns.google/resolve":
-            query_url = f"{doh_serv}?name={ran_website}&type=a&do=1"
-            response = requests.get(query_url)
+# Cleaned list of websites. Avoid prefixes like 'www.' or 'http://'
+# to ensure they work correctly with both DoH queries and browser navigation.
+WEBSITES = [
+    "google.com",
+    "youtube.com",
+    "facebook.com",
+    "amazon.com",
+    "twitter.com",
+    "instagram.com",
+    "linkedin.com",
+    "pinterest.com",
+    "reddit.com",
+    "wikipedia.org",
+    "netflix.com",
+    "microsoft.com",
+    "ebay.com",
+    "nike.com",
+    "apple.com",
+    "yahoo.com",
+    "twitch.tv",
+    "vimeo.com",
+    "dropbox.com",
+    "airbnb.com",
+    "spotify.com",
+    "snapchat.com",
+    "hulu.com",
+    "walmart.com",
+    "github.com",
+    "slack.com",
+    "medium.com",
+    "canva.com",
+    "shopify.com",
+    "zillow.com",
+    "fandom.com",
+    "paypal.com",
+    "cnn.com",
+    "bbc.com",
+    "espn.com",
+    "forbes.com",
+    "cnbc.com",
+    "nytimes.com",
+    "huffpost.com",
+    "buzzfeed.com",
+    "nationalgeographic.com",
+    "stackoverflow.com",
+    "theguardian.com",
+    "mashable.com",
+    "indeed.com",
+    "engadget.com",
+    "wired.com",
+    "cnet.com",
+]
 
-        # If the servers is cloudflares it sets the header and params to the correct values
-        # and preforms the lookup
-        else:
-            header = {"accept": "application/dns-json"}
-            params = {    "name": ran_website, "type": "AAAA"}
-            response = requests.get(doh_serv, params= params, headers= header)
-            
-        
-        try:
-
-            #-------------------------------------------------------------------------------------------------------
-              # This is used for error checking. If the request was successful it will print out the
-              # information received, if there is an request error it will print out the status_code for that error
-            #-------------------------------------------------------------------------------------------------------
-
-             if response.status_code == 200:
-                   print("Response Content:")
-                   print (response.content)
-            
-            # If encounters an error prints out the status code
-             else:
-              print(f"Request failed with status code: {response.status_code}")
-
-     #-------------------------------------------------------------------------------------------------------
-      # Again this is used for error checking if there is an error during the lookup process it will output
-      # the error
-     #------------------------------------------------------------------------------------------------------
-        except Exception as error:
-            print("DoH lookup err:", error)
-
-    else:
-        print("Performing non-DoH lookup:", ran_website)
-
-        try:
-           # Preforms the non-doH lookup
-           print("Response Content:")
-           response = requests.get(f"https://"+ran_website)
-           print(response.content)
-        
-        # Will show the error if one occurs
-        except Exception as e:
-            print("Non-DoH lookup err:", e)
+# --- Function Definitions ---
 
 
-# Close the Chrome driver
-driver.quit()
+def perform_doh_lookup(website):
+    """
+    Performs a DoH lookup for a given website using a randomly chosen DoH server.
+    """
+    print(f"Performing DoH lookup for: {website}")
+    try:
+        server_config = random.choice(DOH_SERVERS)
+        params = server_config["params"](website)
+
+        response = requests.get(
+            server_config["url"],
+            params=params,
+            headers=server_config["headers"],
+            timeout=10,  # Add a timeout to prevent hanging
+        )
+        response.raise_for_status()
+        print("Response Content:")
+        print(response.content)
+    except RequestException as e:
+        print(f"DoH lookup failed: {e}")
+
+
+def perform_nondoh_lookup(website):
+    """
+    Performs a non-DoH lookup by navigating to the website using Selenium,
+    which triggers a standard system DNS lookup.
+    """
+    print(f"Performing non-DoH lookup for: {website}")
+    try:
+        response = requests.get(
+            f"https://{website}",
+            timeout=10,
+        )
+        response.raise_for_status()
+        print("Response Content:")
+        print(response.content)
+    except RequestException as e:
+        print(f"Non- DoH lookup failed: {e}")
+
+
+# --- Main Execution Block ---=
+def main():
+    """
+    Main function to set up the driver and run the traffic generation loop.
+    """
+    driver = None  # Initialize driver to None
+    try:
+        for i in range(NUM_REQUESTS):
+            website = random.choice(WEBSITES)
+
+            if random.uniform(0, 100) < DOH_PROBABILITY_PERCENT:
+                perform_doh_lookup(website)
+            else:
+                perform_nondoh_lookup(website)
+
+    finally:
+        if driver:
+            print("\nShutting down the Chrome driver.")
+            driver.quit()
+        print("Script finished.")
+
+
+if __name__ == "__main__":
+    main()
